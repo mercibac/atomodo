4fce89327e9ebbf70fe18569f243036f70616667
mercibac <mercibacishoge@gmail.com>
2023-09-11 18:15:02 +0000

    make this app a singleton

--- src/Atomodo.dpr
+++ src/Atomodo.dpr
@@ -7,18 +7,40 @@ uses
   main in 'main.pas' {MainForm},
   about in 'about.pas' {AboutForm},
   uMyMutex in 'uMyMutex.pas',
+  System.Classes,
+  System.SysUtils,
   Vcl.Forms,
   Vcl.Themes,
   Vcl.Styles;
 
 {$R *.res}
+
+var
+  LPidFileName: string;
+
 begin
-  Application.Initialize;
-  Application.CreateForm(TUtilModule, UtilModule);
+  LPidFileName := 'Atomodo';
+  try
+
+    with TMutex.Create(LPidFileName) do
+    try
+      begin
+        Application.Initialize;
+        Application.CreateForm(TUtilModule, UtilModule);
   UtilModule.Language.setSource('language_rc');
-  Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TMainForm, MainForm);
-  Application.CreateForm(TAboutForm, AboutForm);
-  Application.Run;
+        Application.MainFormOnTaskbar := True;
+        Application.CreateForm(TMainForm, MainForm);
+        Application.CreateForm(TAboutForm, AboutForm);
+        Application.Run;
+      end
+    finally
+      Free;
+    end;
+  except
+    on E: Exception do
+    begin
+      Application.Restore;
+    end;
+  end;
 end.
 
